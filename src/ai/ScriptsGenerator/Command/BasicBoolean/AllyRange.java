b8506b3f0905a95cf3fa9f05a3765eb7a1b790cb
Julian Mariño <richardes71@gmail.com>
2018-10-08 00:47:44 -0300

    New booleans added

---
+++ src/ai/ScriptsGenerator/Command/BasicBoolean/AllyRange.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai.ScriptsGenerator.Command.BasicBoolean;
+
+import ai.ScriptsGenerator.Command.AbstractBasicAction;
+import ai.ScriptsGenerator.Command.AbstractBooleanAction;
+import ai.ScriptsGenerator.Command.BasicAction.AttackBasic;
+import ai.ScriptsGenerator.Command.Enumerators.EnumTypeUnits;
+import ai.ScriptsGenerator.CommandInterfaces.ICommand;
+import ai.ScriptsGenerator.IParameters.IBehavior;
+import ai.ScriptsGenerator.IParameters.IParameters;
+import ai.ScriptsGenerator.ParametersConcrete.ClosestEnemy;
+import ai.ScriptsGenerator.ParametersConcrete.IPlayerTargetParam;
+import ai.ScriptsGenerator.ParametersConcrete.TypeConcrete;
+import ai.ScriptsGenerator.ParametersConcrete.UnitTypeParam;
+import ai.abstraction.AbstractAction;
+import ai.abstraction.Attack;
+import ai.abstraction.pathfinding.PathFinding;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import rts.GameState;
+import rts.PhysicalGameState;
+import rts.PlayerAction;
+import rts.ResourceUsage;
+import rts.UnitAction;
+import rts.units.Unit;
+import rts.units.UnitTypeTable;
+
+/**
+ *
+ * @author rubens Julian
+ * This condition evaluates if some unitAlly is in the attack range of an enemy
+ */
+public class AllyRange extends AbstractBooleanAction {
+
+	
+    @Override
+    public PlayerAction getAction(GameState game, int player, PlayerAction currentPlayerAction, PathFinding pf, UnitTypeTable a_utt) {
+        ResourceUsage resources = new ResourceUsage();
+        PhysicalGameState pgs = game.getPhysicalGameState();
+        
+        //ActionsInstantation
+        //ActionsInstantation();
+
+        
+        //update variable resources
+        resources = getResourcesUsed(currentPlayerAction, pgs);
+       for(Unit unAlly : getPotentialUnits(game, currentPlayerAction, player)){
+           for (Unit u2 : pgs.getUnits()) {
+        	   
+               if (u2.getPlayer() >= 0 && u2.getPlayer() != player) {
+            	   
+                   int dx = u2.getX()-unAlly.getX();
+                   int dy = u2.getY()-unAlly.getY();
+                   double d = Math.sqrt(dx*dx+dy*dy);
+                   
+                   if (d<=unAlly.getAttackRange()) {              	   
+                	   //appendCommands(player, game, currentPlayerAction);
+                   }
+               }            
+           }
+        }
+        return currentPlayerAction;
+    }
+
+
+    private Iterable<Unit> getPotentialUnits(GameState game, PlayerAction currentPlayerAction, int player) {
+        ArrayList<Unit> unitAllys = new ArrayList<>();
+        for (Unit u : game.getUnits()) {
+            if(u.getPlayer() == player && currentPlayerAction.getAction(u) == null 
+                    && game.getActionAssignment(u) == null && u.getResources() == 0
+                    && isUnitControlledByParam(u)){
+                unitAllys.add(u);
+            }
+        }
+        return unitAllys;
+    }
+
+    private boolean isUnitControlledByParam(Unit u) {
+        List<UnitTypeParam> unType = getTypeUnitFromParam();
+        for (UnitTypeParam unitTypeParam : unType) {
+        	
+            for (EnumTypeUnits paramType : unitTypeParam.getParamTypes()) {
+                if(u.getType().ID == paramType.code()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
