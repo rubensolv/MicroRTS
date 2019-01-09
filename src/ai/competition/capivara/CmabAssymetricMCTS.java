34cebae802fcfb238569aead2a3cc13bc686d46b
Julian Mariño <richardes71@gmail.com>
2019-01-09 11:40:34 -0500

    CHanges for new configurations of new GA scripts generator

--- src/ai/competition/capivara/CmabAssymetricMCTS.java
+++ src/ai/competition/capivara/CmabAssymetricMCTS.java
@@ -415,7 +415,8 @@ public class CmabAssymetricMCTS extends AIWithComputationBudget implements Inter
     @Override
     public String toString() {
         //return getClass().getSimpleName() + "(" + TIME_BUDGET + ", " + ITERATIONS_BUDGET + ", " + MAXSIMULATIONTIME + "," + MAX_TREE_DEPTH + "," + epsilon_l + ", " + discount_l + ", " + epsilon_g + ", " + discount_g + ", " + epsilon_0 + ", " + discount_0 + ", " + playoutPolicy + ", " + ef + ")";
-        return this.name;
+        return "A3N_"+name;
+    	//return this.name;
     }
     
     @Override
