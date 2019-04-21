b73b9740dda5802bb6a3a4a8e447ab58c50d76e3
rubensolv <rubens_olv@hotmail.com>
2019-04-21 12:01:49 -0300

    included AbstractConditionalFunction

---
+++ src/ai/ScriptsGenerator/BasicConditional/functions/AbstractConditionalFunction.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai.ScriptsGenerator.BasicConditional.functions;
+
+import ai.ScriptsGenerator.Command.Enumerators.EnumTypeUnits;
+import ai.ScriptsGenerator.IParameters.IParameters;
+import ai.ScriptsGenerator.ParametersConcrete.UnitTypeParam;
+import java.util.ArrayList;
+import java.util.List;
+import rts.GameState;
+import rts.PlayerAction;
+import rts.units.Unit;
+
+/**
+ *
+ * @author rubens
+ */
+public abstract class AbstractConditionalFunction implements IConditionalFunction{
+    protected List<IParameters> parameters = new ArrayList<>();
+    
+    protected List<IParameters> getParameters() {
+        return parameters;
+    }
+    
+    protected Iterable<Unit> getPotentialUnits(GameState game, PlayerAction currentPlayerAction, int player) {
+        ArrayList<Unit> unitAllys = new ArrayList<>();
+        for (Unit u : game.getUnits()) {
+            if(u.getPlayer() == player && currentPlayerAction.getAction(u) == null 
+                    && game.getActionAssignment(u) == null && u.getResources() == 0
+                    && isUnitControlledByParam(u)){
+                unitAllys.add(u);
+            }
+        }
+        return unitAllys;
+    }
+    protected boolean isUnitControlledByParam(Unit u) {
+        List<UnitTypeParam> unType = getTypeUnitFromParam();
+        for (UnitTypeParam unitTypeParam : unType) {
+        	
+            for (EnumTypeUnits paramType : unitTypeParam.getParamTypes()) {
+                if(u.getType().ID == paramType.code()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    protected List<UnitTypeParam> getTypeUnitFromParam() {
+        List<UnitTypeParam> types = new ArrayList<>();
+        for (IParameters param : getParameters()) {
+            if(param instanceof UnitTypeParam){
+                types.add((UnitTypeParam) param);
+            }
+        }
+        return types;
+    }
+}
