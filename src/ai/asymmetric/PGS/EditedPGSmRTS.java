74971c7ec0ccc769e8d406e590196f545150572f
Julian Mariño <richardes71@gmail.com>
2020-08-26 21:28:06 -0300

    Minnor change

--- src/ai/asymmetric/PGS/EditedPGSmRTS.java
+++ src/ai/asymmetric/PGS/EditedPGSmRTS.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 import rts.GameState;
 import rts.PlayerAction;
 import rts.UnitAction;
@@ -227,7 +226,7 @@ public class EditedPGSmRTS extends AIWithComputationBudget implements Interrupti
      * @param gs
      * @param uScriptPlayer
      * @param aiEnemy
-     * @return a avaliaÃ§Ã£o para ser utilizada como base.
+     * @return a avaliação para ser utilizada como base.
      * @throws Exception
      */
     public double eval(int player, GameState gs, UnitScriptData uScriptPlayer, AI aiEnemy) throws Exception {
@@ -350,7 +349,7 @@ public class EditedPGSmRTS extends AIWithComputationBudget implements Interrupti
         UnitScriptData bestScriptData = currentScriptData.clone();
         double bestScore = eval(player, gs_to_start_from, bestScriptData, seedEnemy);
         ArrayList<Unit> unitsPlayer = getUnitsPlayer(player);
-        //controle pelo nÃºmero de iteraÃ§Ãµes
+        //controle pelo número de iterações
         //for (int i = 0; i < I; i++) {
         //controla por tempo
         while (System.currentTimeMillis() < (start_time + (TIME_BUDGET - 8))) {
@@ -377,7 +376,7 @@ public class EditedPGSmRTS extends AIWithComputationBudget implements Interrupti
                         return bestScriptData.clone();
                     }
                 }
-                //seto o melhor vetor para ser usado em futuras simulaÃ§Ãµes
+                //seto o melhor vetor para ser usado em futuras simulações
                 currentScriptData = bestScriptData.clone();
             }
         }
@@ -461,4 +460,3 @@ public class EditedPGSmRTS extends AIWithComputationBudget implements Interrupti
     }
 
 }
-
